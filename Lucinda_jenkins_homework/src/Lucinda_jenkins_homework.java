4908b886a63bc2ead8c156c6b9acf29b49cc21a1
lucindaLin <88137977+lucindaLin@users.noreply.github.com>
2021-07-29 21:25:09 +0800

    Update Lucinda_jenkins_homework.java

--- Lucinda_jenkins_homework/src/Lucinda_jenkins_homework.java
+++ Lucinda_jenkins_homework/src/Lucinda_jenkins_homework.java
@@ -2,16 +2,15 @@ import java.util.*;
 public class Lucinda_jenkins_homework {
     public static void main(String[] args){
         System.out.println("请输入需要相加的数x：");
-/*
+
         Scanner reader=new Scanner(System.in);
 
         double x = reader.nextDouble();
 
         System.out.println("请输入需要相加的数y：");
 
-        double y = reader.nextDouble();*/
-        double x = 3;
-        double y = 4.3;
+        double y = reader.nextDouble();
+
         double sum = x + y;
 
         System.out.print("输入的和为："+sum);
